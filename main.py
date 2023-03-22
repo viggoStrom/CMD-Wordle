30d790fa0e12d22792ad4a677877902107c70f90
viggoStrom <104303404+viggoStrom@users.noreply.github.com>
2023-03-22 15:28:27 +0100

    base structure

---
+++ main.py
@@ -0,0 +1,12 @@
+class main:
+    def __init__(self):
+        
+        pass
+
+    def run(self):
+
+        pass
+
+main = main()
+
+main.run()
---
+++ textHandler.py
@@ -0,0 +1,18 @@
+class text:
+    def __init__(self):
+        
+        # create file if it doesnt exist
+        # else 
+        # open existing file 
+
+        # structure of file (csv)
+        # index, name, score
+        pass
+
+    def read(self, person):
+        
+        return
+
+    def write(self, person):
+        # if person alrady has score ask if they wanna override it 
+        return
